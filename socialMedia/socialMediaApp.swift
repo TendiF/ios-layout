7f2e15732dbebc1b5a9e6b5a8273a0991d9e06f6
Tendi F <tendifirmansyah30@gmail.com>
2020-10-24 12:54:03 +0700

    Initial Commit

---
+++ socialMedia/socialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  socialMediaApp.swift
+//  socialMedia
+//
+//  Created by Macbook Pro on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct socialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
